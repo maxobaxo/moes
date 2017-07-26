93713ed9730b4493b344cc43b35c84429577ddf8
jacob ruleaux and Michelle Poterek <michelle@>
2017-07-26 11:25:20 -0700

    add future functionality to see product order review

--- app/app.php
+++ app/app.php
@@ -106,5 +106,28 @@
         return $app['twig']->render('store.html.twig', array('products' => Product::getAll(), 'product' => $product));
     });
 
+    $app->post("/review_order/{id}", function($id) use ($app) {
+        $customer = Customer::find($id);
+        $cart = Cart::find($id);
+        return $app['twig']->render('review_order.html.twig', array('cart' => $cart, 'cart_products' => $cart->getProducts(), 'customer' => $customer));
+    });
+
+    // $app->get("/cart/{id}/edit", function($id) use ($app) {
+    //     $cart = Cart::find($id);
+    //     return $app['twig']->render('review_order.html.twig', array('cart' => $cart));
+    // });
+    //
+    // $app->patch("/cart/{id}", function($id) use ($app) {
+    //     $cart = Cart::find($id);
+    //     $cart->update($cart);
+    //     return $app['twig']->render('review_order.html.twig', array('carts' => Cart::getAll()));
+    // });
+    //
+    // $app->post("/clear_cart", function($id) use ($app) {
+    //     $cart = Cart::find($id);
+    //     Cart::delete();
+    //     return $app['twig']->render('store.html.twig');
+    // });
+
     return $app
 ?>
